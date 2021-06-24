f7f9b34a3945fb0bd6b095611537ffe9dbd84fa8
Stephan Kolontay <85121881+stfn-ko@users.noreply.github.com>
2021-06-24 21:09:09 +0300

    Update GMC.go

--- GMC.go
+++ GMC.go
@@ -6,8 +6,6 @@ import (
 	"time"
 )
 
-/* == CONSTANTS == */
-
 const (
 	NoExpiration      time.Duration = -1
 	DefaultExpiration time.Duration = 0
@@ -21,16 +19,11 @@ type GenericMemoryCache interface {
 	Delete(key string)
 }
 
-type CACHE struct {
-	*cache_line 
-}
-
-type cache_line struct {
-	sync.RWMutex                           	//reader/writer mutual exclusion lock
-	onEvicted    func(string, interface{}) 	//key and data of evicted entry
-	cleanUP      *cleanUP                  	//mr.proper (autocleans cache)
-	entries      map[string]Entry           //entries collection
-	ttl          time.Duration             	//time to live
+type cacheLine struct {
+	sync.RWMutex                  //reader/writer mutual exclusion lock
+	cleanUP      *cleanUP         //mr.proper (autocleans cache)
+	entries      map[string]Entry //entries collection
+	ttl          time.Duration    //time to live
 }
 
 type Entry struct {
@@ -41,11 +34,6 @@ type Entry struct {
 type cleanUP struct {
 	Interval  time.Duration
 	notActive chan bool
-}
-
-type key_and_value struct {
-	value interface{}
-	key   string
 }
 
 /* == FUNCTIONS & METHODS == */
@@ -60,19 +48,19 @@ func (entry Entry) Expired() bool {
 
 // Gets an entriy from the cahce. If key was found --> entry & true.
 // Otherwise --> nil & false
-func (cache *CACHE) Get(key string) (interface{}, bool) {
-	cache.RLock() 							// <-- LOCK
+func (cache *cacheLine) Get(key string) (interface{}, bool) {
+	cache.RLock() // <-- LOCK
 
 	entry, found := cache.entries[key]
 	if !found {
-		cache.RUnlock() 					// <-- UNLOCK
+		cache.RUnlock() // <-- UNLOCK
 		return nil, false
 	}
 	if entry.Expired() {
-		cache.RUnlock()						// <-- UNLOCK
+		cache.RUnlock() // <-- UNLOCK
 		return nil, false
 	}
-	cache.RUnlock() 						// <-- UNLOCK
+	cache.RUnlock() // <-- UNLOCK
 	return entry.Data, true
 }
 
@@ -80,7 +68,7 @@ func (cache *CACHE) Get(key string) (interface{}, bool) {
 // If ttl == DefaultExpiration (0), the default expiration time is used.
 // Else if ttl == NoExpiration (-1), the data never expires.
 // Else if ttl != (DefaultExpiration || NoExpiration), the expiration time is set to a custom one.
-func (cache*CACHE) Set(key string, data interface{}, ttl time.Duration) {
+func (cache *cacheLine) Set(key string, data interface{}, ttl time.Duration) {
 	var exp int64
 
 	if ttl == DefaultExpiration {
@@ -90,65 +78,57 @@ func (cache*CACHE) Set(key string, data interface{}, ttl time.Duration) {
 		exp = time.Now().Add(ttl).UnixNano()
 	}
 
-	cache.Lock() 							// <-- LOCK
+	cache.Lock() // <-- LOCK
 	cache.entries[key] = Entry{
 		Expiration: exp,
-		Data:      	data,
+		Data:       data,
 	}
-	cache.Unlock() 							// <-- UNLOCK
+	cache.Unlock() // <-- UNLOCK
 }
 
 // Delete's subroutine. Actually deletes the entry from cache line. Returns deleted entry's key and truth if requested key exists. Otherwise returns nil and false
-func (cl *cache_line) delete(key string) (interface{}, bool) {
-	
-	if cl.onEvicted != nil {
-		if entry, found := cl.entries[key]; found {
-			delete(cl.entries, key)
-			return entry.Data, true
-		}
+func (cl *cacheLine) delete(key string) (interface{}, bool) {
+
+	//if cl.onEvicted != nil {
+	if entry, found := cl.entries[key]; found {
+		delete(cl.entries, key)
+		return entry.Data, true
 	}
+	//}
 	delete(cl.entries, key)
 	return nil, false
 }
 
 // Deletes entry from the cache line and puts it on evicted line. Otherwise, if the key doesnt match the request does nothing.
-func (cl *cache_line) Delete(key string) {
+func (cl *cacheLine) Delete(key string) {
 
-	cl.Lock() 								// <-- LOCK
-	entry, evicted := cl.delete(key)
-	cl.Unlock() 							// <-- UNLOCK
-	if evicted {
-		cl.onEvicted(key, entry)
-	}
+	cl.Lock() // <-- LOCK
+	cl.delete(key)
+	cl.Unlock() // <-- UNLOCK
+
 }
 
 // Deletes all expired entries from cache line. Updates onEvicted using LRU
-func (cl *cache_line) DeleteExpired() {
-	var evicted_Entries []key_and_value
+func (cl *cacheLine) DeleteExpired() {
+
 	cl.Lock() // <-- LOCK
 	for key, value := range cl.entries {
-		if value.Expired(){
-			entry, evicted := cl.delete(key)
-			if evicted {
-				evicted_Entries = append(evicted_Entries, key_and_value{key: key, value: entry})
-			}
+		if value.Expired() {
+			cl.delete(key)
 		}
 	}
-	cl.Unlock() 							// <-- UNLOCK
-	for _, entry := range evicted_Entries {
-		cl.onEvicted(entry.key, entry.value)
-	}
+	cl.Unlock() // <-- UNLOCK
 }
 
-// Stops cleaner routine 
-func deactivatecacheeaner(cache *CACHE) {
+// Stops cleaner routine
+func stopCleaner(cache *cacheLine) {
 	cache.cleanUP.notActive <- true
 }
 
 // Runs a cleanup routine
-func (cleaner *cleanUP) Run(cL *cache_line) {
+func (cleaner *cleanUP) Run(cL *cacheLine) {
 	ticker := time.NewTicker(cleaner.Interval)
-	
+
 	for {
 		select {
 		case <-ticker.C:
@@ -161,7 +141,7 @@ func (cleaner *cleanUP) Run(cL *cache_line) {
 }
 
 // Starts cleaner routine
-func activatecacheeaner(cl *cache_line, cUP time.Duration) {
+func startCleaner(cl *cacheLine, cUP time.Duration) {
 	cleaner := &cleanUP{
 		Interval:  cUP,
 		notActive: make(chan bool),
@@ -170,33 +150,33 @@ func activatecacheeaner(cl *cache_line, cUP time.Duration) {
 	go cleaner.Run(cl)
 }
 
-// new_Autoclean_Cache()'s subroutine. Creates new default cache line ready to get set to new values. 
-func new_Cache_line(def_exp time.Duration, new_entry map[string]Entry) *cache_line {
+// new_Autoclean_Cache()'s subroutine. Creates new default cache line ready to get set to new values.
+func new_cacheLine(def_exp time.Duration, new_entry map[string]Entry) *cacheLine {
 	if def_exp == DefaultExpiration {
 		def_exp = NoExpiration
 	}
 
-	cl := &cache_line{
+	cl := &cacheLine{
 		entries: new_entry,
-		ttl:   def_exp,
+		ttl:     def_exp,
 	}
 	return cl
 }
 
 // New()'s subroutine. Creates a new cache with automatic clean up. If clean up interval set to default (0) or
 // no-expiration (-1), the autoclean routine doesn't start
-func new_Autoclean_Cache(def_exp, cUP time.Duration, new_entry map[string]Entry) *CACHE {
-	cl := new_Cache_line(def_exp, new_entry)
-	cache := &CACHE{cl}
+func new_Autoclean_Cache(def_exp, cUP time.Duration, new_entry map[string]Entry) *cacheLine {
+	cl := new_cacheLine(def_exp, new_entry)
+	cache := cl
 	if cUP > 0 {
-		activatecacheeaner(cl, cUP)
-		runtime.SetFinalizer(cache, deactivatecacheeaner)
+		startCleaner(cl, cUP)
+		runtime.SetFinalizer(cache, stopCleaner)
 	}
 	return cache
 }
 
 // Cache constructor
-func New(def_exp, cUP time.Duration) *CACHE {
+func New(def_exp, cUP time.Duration) *cacheLine {
 	entry := make(map[string]Entry)
 	return new_Autoclean_Cache(def_exp, cUP, entry)
 }
